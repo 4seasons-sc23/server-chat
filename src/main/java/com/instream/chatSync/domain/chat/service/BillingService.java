ca288af6c72a4bc7eadc30f35090021bf939e1a8
Yoo Jin <dbwls30131@ajou.ac.kr>
2023-12-02 19:29:27 +0900

    <FIX> WebClient baseUrl 설정

--- src/main/java/com/instream/chatSync/domain/chat/service/BillingService.java
+++ src/main/java/com/instream/chatSync/domain/chat/service/BillingService.java
@@ -1,6 +1,7 @@
 package com.instream.chatSync.domain.chat.service;
 
 import com.instream.chatSync.domain.chat.domain.dto.request.ChatBillingRequestDto;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
@@ -11,19 +12,25 @@ import reactor.core.publisher.Mono;
 @Service
 public class BillingService {
 
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
+    private WebClient webClient;
 
     @Value("${tenant.base-url}")
     private String TENANT_BASE_URL;
 
     @Autowired
     public BillingService(WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+    }
+
+    @PostConstruct
+    public void init() {
         this.webClient = webClientBuilder.baseUrl(TENANT_BASE_URL).build();
     }
 
     public Mono<HttpStatusCode> postChatBilling(ChatBillingRequestDto chatBillingRequestDto) {
         return webClient.post()
-            .uri("/v1/billings")
+            .uri("/api/v1/billings")
             .body(Mono.just(chatBillingRequestDto), ChatBillingRequestDto.class)
             .exchangeToMono(response -> Mono.just(response.statusCode()));
     }
