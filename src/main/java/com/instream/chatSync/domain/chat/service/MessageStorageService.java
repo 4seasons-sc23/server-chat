08c8f77607f8dd47e1f3180d0b2724cdbc33ca7e
Long9725 <develop9725@gmail.com>
2023-12-11 11:56:05 +0900

    <REFACTOR> addPublishFlux method thread-safe

--- src/main/java/com/instream/chatSync/domain/chat/service/MessageStorageService.java
+++ src/main/java/com/instream/chatSync/domain/chat/service/MessageStorageService.java
@@ -6,7 +6,6 @@ import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Service;
 import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 
 import java.time.Duration;
@@ -42,36 +41,12 @@ public class MessageStorageService {
         if (messagePublishFluxes.containsKey(sessionId)) {
             return;
         }
-        Flux<Void> flux = Flux.interval(Duration.ofSeconds(1))
-                .flatMap(tick -> {
-                    Queue<String> msgQueue = messageQueues.get(sessionId);
+        Flux<Long> flux = Flux.interval(Duration.ofSeconds(1)).doOnNext(tick -> publishMessages(sessionId));
 
-                    if (msgQueue == null || msgQueue.isEmpty()) {
-                        return Mono.empty();
-                    }
-
-                    // 임시 메세지 저장소 생성
-                    List<String> tempMessages = new ArrayList<>(msgQueue);
-
-                    // messageQueue를 비워줍니다. 채팅 편집 동시성을 높이기 위해서 원본은 사용하지 않습니다.
-                    msgQueue.clear();
-
-                    billingChat(sessionId, tempMessages);
-
-                    // sessionId 해당하는 소켓에 메시지 전송
-                    ServerSentEvent<List<String>> event = ServerSentEvent.builder(tempMessages).build();
-                    sessionSockets.getOrDefault(sessionId, new ArrayList<>())
-                            .parallelStream()
-                            .forEach(socket -> socket.tryEmitNext(event));
-
-                    // 임시 메시지 큐 메모리 해제
-                    tempMessages.clear();
-                    return Mono.empty();
-                });
-
-        log.info("Create message publisher {}", sessionId);
-
-        messagePublishFluxes.put(sessionId, flux.subscribe());
+        messagePublishFluxes.computeIfAbsent(sessionId, key -> {
+            log.info("Create message publisher {}", sessionId);
+            return flux.subscribe();
+        });
     }
 
     public Flux<ServerSentEvent<List<String>>> streamMessages(UUID sessionId) {
@@ -82,6 +57,31 @@ public class MessageStorageService {
         log.info("Create SSE {}", sessionId);
 
         return sink.asFlux();
+    }
+
+    private void publishMessages(UUID sessionId) {
+        Queue<String> msgQueue = messageQueues.get(sessionId);
+
+        if (msgQueue == null || msgQueue.isEmpty()) {
+            return;
+        }
+
+        // 임시 메세지 저장소 생성
+        List<String> tempMessages = new ArrayList<>(msgQueue);
+
+        // messageQueue를 비워줍니다. 채팅 편집 동시성을 높이기 위해서 원본은 사용하지 않습니다.
+        msgQueue.clear();
+
+        billingChat(sessionId, tempMessages);
+
+        // sessionId 해당하는 소켓에 메시지 전송
+        ServerSentEvent<List<String>> event = ServerSentEvent.builder(tempMessages).build();
+        sessionSockets.getOrDefault(sessionId, new ArrayList<>())
+                .parallelStream()
+                .forEach(socket -> socket.tryEmitNext(event));
+
+        // 임시 메시지 큐 메모리 해제
+        tempMessages.clear();
     }
 
     private void billingChat(UUID sessionId, List<String> tempMessages) {
